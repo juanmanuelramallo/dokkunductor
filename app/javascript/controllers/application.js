960ed1b412ff077c423db3b5b3111655f6ec5092
Juan Manuel Ramallo <juanmanuelramallo@hey.com>
2022-11-04 00:36:57 -0300

    [terminal] Added a terminal at the bottom of the screen to serve as a log of all commands executed against the host dokku machine

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,11 +1,13 @@
 import { Application } from "@hotwired/stimulus"
 import Clipboard from "stimulus-clipboard"
 import Notification from "stimulus-notification";
+import ScrollIntoView from "./scroll_into_view_controller";
 
 const application = Application.start()
 
 application.register("clipboard", Clipboard)
 application.register("notification", Notification)
+application.register("scroll-into-view", ScrollIntoView)
 
 // Configure Stimulus development experience
 application.debug = false
