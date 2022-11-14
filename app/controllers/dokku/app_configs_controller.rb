ddc4e59c7a6e639935c778735c6c581d0be607a1
Juan Manuel Ramallo <juanmanuelramallo@hey.com>
2022-11-13 22:51:23 -0300

    [config] Ability to update and add new config vars

--- app/controllers/dokku/app_configs_controller.rb
+++ app/controllers/dokku/app_configs_controller.rb
@@ -4,5 +4,26 @@ module Dokku
       @app = Dokku::App.new(name: params[:app_id])
       @config = @app.config
     end
+
+    def update
+      @app = Dokku::App.new(name: params[:app_id])
+      @config = @app.config
+
+      configs = config_params.to_h
+      new_name, new_value = configs.delete(:new_name), configs.delete(:new_value)
+      configs.merge!(new_name => new_value) if new_name.present? && new_value.present?
+
+      if @app.update_config(configs)
+        redirect_to dokku_app_app_configs_path(app_id: @app.name), notice: "Config updated"
+      else
+        render :show, status: :see_other
+      end
+    end
+
+    private
+
+    def config_params
+      params.require(:config).permit!
+    end
   end
 end
