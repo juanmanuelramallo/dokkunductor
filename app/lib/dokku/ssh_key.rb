7d35494cde3e142c520cda42c114b52c307c0fae
Juan Manuel Ramallo <juanmanuelramallo@hey.com>
2022-11-19 18:13:40 -0300

    [ssh-key] Add Dokku::SshKey.create

--- app/lib/dokku/ssh_key.rb
+++ app/lib/dokku/ssh_key.rb
@@ -2,7 +2,7 @@ module Dokku
   class SshKey
     include ActiveModel::Model
 
-    attr_accessor :info
+    attr_accessor :info, :name, :public_key
 
     class << self
       def all
@@ -11,6 +11,21 @@ module Dokku
         result.split("\n").sort.map do |text|
           new(info: text)
         end
+      end
+
+      # @param args [Hash]
+      #   - :name [String] The name of the key
+      #   - :public_key [String] The public key
+      def create(args)
+        result = Ssh.new.exec("echo \"#{args[:public_key]}\" | ssh-keys:add #{args[:name]}")
+
+        ssh_key = new(name: args[:name], public_key: args[:public_key])
+        if result.include?("!")
+          ssh_key.errors.add(:base, result)
+        else
+          ssh_key.info = result
+        end
+        ssh_key
       end
     end
   end
